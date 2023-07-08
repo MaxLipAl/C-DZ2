128d11ecbf337f2ada13dd2e0af2191b6eff61bf
Maxim <maxl08@list.ru>
2023-07-08 21:12:40 +0300

    dz

---
+++ 1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine ());
+int number1 = number;
+
+if (number >= 100 && number <=999)
+{
+    number = (number / 10) % 10;
+    Console.WriteLine ($"{number1} -> {number}");
+}
+else
+{
+    Console.WriteLine("Надо было ввести трехзначное число.");
+}
+
+
+
---
+++ 2/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine ());
+int number1 = number;
+
+if (number / 100 < 1)
+{
+    Console.WriteLine($"{number} -> третьей цифры нет");
+}
+else 
+{
+    if (number / 100 < 10)
+    {
+        Console.WriteLine($"{number} -> {number % 10}");
+    }
+else
+{
+    while (number / 100 > 10)
+    {
+        number = number / 10;
+    }
+    Console.WriteLine($"{number1} -> {number % 10}");
+}
+}
